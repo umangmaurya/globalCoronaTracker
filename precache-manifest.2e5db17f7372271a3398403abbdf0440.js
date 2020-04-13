19eb68dbb47cc498cb801dabd2c75ef934e82634
Umang Maurya <umangmaurya@outlook.com>
2020-04-13 21:38:39 +0530

    Updates

---
+++ precache-manifest.2e5db17f7372271a3398403abbdf0440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1cdc0671de19cbc923505559acc690",
+    "url": "/globalCoronaTracker/index.html"
+  },
+  {
+    "revision": "84a7a0cbd7da437c9c45",
+    "url": "/globalCoronaTracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "12a384dbe3d5f5c37e18",
+    "url": "/globalCoronaTracker/static/js/2.f461d3f6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/globalCoronaTracker/static/js/2.f461d3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a7a0cbd7da437c9c45",
+    "url": "/globalCoronaTracker/static/js/main.94ac65a2.chunk.js"
+  },
+  {
+    "revision": "9ae80cc40b130cf81bee",
+    "url": "/globalCoronaTracker/static/js/runtime-main.12b2843e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/globalCoronaTracker/static/media/image.d7265326.png"
+  }
+]);
