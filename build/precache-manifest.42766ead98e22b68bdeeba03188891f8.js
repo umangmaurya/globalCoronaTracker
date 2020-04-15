b9ca2e62dddaba18fa8e10c62f10ddc51b3dda2f
umangmaurya <umangmaurya@outlook.com>
2020-04-15 16:14:04 +0530

    updated filter

---
+++ build/precache-manifest.42766ead98e22b68bdeeba03188891f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92f8a5cdd43b047ce7d9f0db266c2a6",
+    "url": "/globalCoronaTracker/index.html"
+  },
+  {
+    "revision": "7219e08d379ed2b58e66",
+    "url": "/globalCoronaTracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "12a384dbe3d5f5c37e18",
+    "url": "/globalCoronaTracker/static/js/2.f461d3f6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/globalCoronaTracker/static/js/2.f461d3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7219e08d379ed2b58e66",
+    "url": "/globalCoronaTracker/static/js/main.c05d1cbc.chunk.js"
+  },
+  {
+    "revision": "9ae80cc40b130cf81bee",
+    "url": "/globalCoronaTracker/static/js/runtime-main.12b2843e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/globalCoronaTracker/static/media/image.d7265326.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ import styles from './App.module.css';
 import image from './images/image.png';
 
 class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
-  }
+    state = {
+        data: {},
+        country: '',
+    }
 
-  async componentDidMount() {
-    const data = await fetchData();
+    async componentDidMount() {
+        const data = await fetchData();
 
-    this.setState({ data });
-  }
+        this.setState({ data });
+    }
 
-  handleCountryChange = async (country) => {
-    const data = await fetchData(country);
+    handleCountryChange = async (country) => {
+        const data = await fetchData(country);
 
-    this.setState({ data, country: country });
-  }
+        this.setState({ data, country: country });
+    }
 
-  render() {
-    const { data, country } = this.state;
+    render() {
+        const { data, country } = this.state;
 
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
-      </div>
-    );
-  }
+        return (
+            <div className={styles.container}>
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <img className={styles.image} src={image} alt="COVID-19" />
+                <Cards data={data} />
+                <Chart data={data} country={country} />
+            </div>
+        );
+    }
 }
 
 export default App;
